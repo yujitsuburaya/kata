b1d9ed0161d008253ec9009b9995b75778669e3b
円谷 雄二 <yuji.tsuburaya@bizreach.co.jp>
2019-09-25 16:25:18 +0900

    平日5-9回のパターンのテスト追加した

--- ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
+++ ex01-business-rules/src/test/java/kata/ex01/DiscountServiceTest.java
@@ -40,6 +40,18 @@ public class DiscountServiceTest {
     }
 
     @Test
+    public void test平日朝夕5から9回割引() {
+        HighwayDrive drive = new HighwayDrive();
+        drive.setEnteredAt(LocalDateTime.of(2016, 3, 31, 23, 0));
+        drive.setExitedAt(LocalDateTime.of(2016, 4, 1, 6, 30));
+        drive.setDriver(driver(9));
+        drive.setVehicleFamily(STANDARD);
+        drive.setRouteType(RURAL);
+
+        assertThat(discountService.calc(drive)).isEqualTo(30);
+    }
+
+    @Test
     public void test休日朝夕は休日割が適用される() {
         HighwayDrive drive = new HighwayDrive();
         drive.setEnteredAt(LocalDateTime.of(2016, 4, 1, 23, 0));
